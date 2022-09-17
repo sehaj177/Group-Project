5083d6584b8a4075fd161bed4e1b8bb280c4d82e
sehaj177 <sehaj177singh@gmail.com>
2022-09-17 11:33:22 +0530

    commit

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import './SearchStyles.css'
+import { useState } from 'react'
 
 import Gold from '../../assets/gold.png'
 
 function Search() {
-  
+    
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '7602777b8bmshd5fae124a5f2a53p166bc4jsnd9fb5f0c5f63',
+                'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+            }
+        };
+        
+        fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=new%20york&locale=en_US&currency=INR', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    };
+    // const bookingApi = (e) => {
+    //     function hotelApi() {
+
+    //     }
+    //     console.log('hello');
+    //     e.preventDefault();
+    // }
+
     return (
         <div name='book' className='search'>
             <div className="container">
                 <div className="left">
-                    <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
-                    <p>Come experience the very pinnacle of luxury Caribbean all-inclusive vacations for couples at Sandals Resorts. Our luxury beach resorts, set along the most gorgeous tropical settings and exquisite beaches in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and Curaçao, feature unlimited gourmet dining, unique bars serving premium liquors and wines, and every land and water sport, including complimentary green fees at our golf resorts and PADI® certified scuba diving at most resorts. If you are planning a wedding, Sandals is the leader in Caribbean destination weddings and honeymoon packages.</p>
+                    <h2>LUXURY INCLUDED VACATIONS </h2>
+                    <p>Come experience the very pinnacle of luxury North India all-inclusive vacations for all at  Resorts. Our luxury beach resorts, set along the most gorgeous tropical settings and exquisite beaches in Rajasthan feature unlimited gourmet dining, unique bars serving premium liquors and wines, and every land and water sport, including complimentary green fees at our golf resorts . If you are planning a wedding, Rajasthan is the leader in North India destination weddings and honeymoon packages.</p>
                     <div className="search-col-2">
                         <div className="box">
                             <div>
@@ -18,24 +43,20 @@ function Search() {
                             </div>
                             <div>
                                 <h3>WORLD'S LEADING</h3>
-                                <p>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+                                <p>ALL-INCLUSIVE COMPANY </p>
                             </div>
                         </div>
                         <div className="box">
                             <div>
-                                <h3>NO ONE INCLUDES MORE</h3>
-                                <p>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+                                <h3>NO ONE INCLUDES MORE PACKAGES</h3>
+                                <p>ALL-INCLUSIVE COMPANY </p>
                                 <button>View Packages</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="right">
-                    {/* <div className="promo">
-                        <h4 className="save">GET AN ADDITIONAL 7% OFF</h4>
-                        <p className="timer">12 HOURS LEFT!</p>
-                        <p className="offers">VIEW ALL CURRENT OFFERS</p>
-                    </div> */}
+                    
                     <form>
                         <div className="input-wrap">
                             <label>Destination</label>
@@ -46,7 +67,7 @@ function Search() {
                                 <option value="1">Uttar Pradesh</option>
                                 <option value="1">Uttarakhand</option>
                                 <option value="1">Rajasthan</option>
-                                
+
                             </select>
                         </div>
                         <div className="date">
@@ -59,12 +80,12 @@ function Search() {
                                 <input type="date" />
                             </div>
                         </div>
-                        <button >Rates & Availabilities </button>
+                        <button onClick={handleClick}>Rates & Availabilities </button>
                     </form>
                 </div>
             </div>
         </div>
-    
+
     )
 }
 
