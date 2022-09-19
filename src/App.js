dab30ba8a4846877a4c44ecf6b200ad231ec7afc
PranavSharma-014 <pranav0539.be20@chitkara.edu.in>
2022-09-19 12:57:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Destinations from './components/destinations/Destinations'
-import Footer from './components/footer/Footer';
-import Hero from './components/hero/Hero'
-import Navbar from './components/navbar/Navbar'
-import Search from './components/search/Search';
-import Selects from './components/selects/Selects';
-import Weather from './components/Weather/Weather'
-import HotelData from './components/HotelData/HotelData'
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
+import Destinations from "./components/destinations/Destinations";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Search from "./components/search/Search";
+import Selects from "./components/selects/Selects";
+import Weather from "./components/Weather/Weather";
+import HotelData from "./components/HotelData/HotelData";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
-const locName = {"1":"New Delhi",
-               "2":"Punjab",
-                "3":"Chandigarh",
-                "4":"Uttar Pradesh",
-                "5":"Uttarakhand",
-                "6":"Rajasthan"}
+const locName = {
+  1: "New Delhi",
+  2: "Punjab",
+  3: "Chandigarh",
+  4: "Uttar Pradesh",
+  5: "Uttarakhand",
+  6: "Rajasthan",
+};
 
 function App() {
   const [isVisible, showdata] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState(1);
 
-  const displayHoteldata = (flag)=>{
-    if(flag)
-      return <HotelData name={locName[selectedLocation]}></HotelData>
-    else
-      return <div></div>
-  }
+  const displayHoteldata = (flag) => {
+    if (flag) return <HotelData name={locName[selectedLocation]}></HotelData>;
+    else return <div></div>;
+  };
 
   return (
     <div>
-
-    <Navbar />
+      <Navbar />
       <BrowserRouter>
         <Hero />
         <Destinations />
@@ -46,12 +40,16 @@ function App() {
             <Route exact path="/destinations" element={<Destinations />} />
             <Route exact path="/selects" element={<Selects />} />
         </Routes> */}
-        <Search visibility={isVisible} displayHotelData={showdata} setSelectedLocation={setSelectedLocation}/>
+        <Search
+          visibility={isVisible}
+          displayHotelData={showdata}
+          setSelectedLocation={setSelectedLocation}
+        />
         {displayHoteldata(isVisible)}
         <Selects />
         <br />
         {/* <ImgCarousel /> */}
-        <Weather/>
+        <Weather />
         <Footer />
       </BrowserRouter>
     </div>
