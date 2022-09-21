d9078282a644b0d45c41f2c1e018af7b94f200ef
PranavSharma-014 <pranav0539.be20@chitkara.edu.in>
2022-09-21 14:51:44 +0530

    Commit

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -7,7 +7,7 @@ import {WiDayShowers} from 'react-icons/wi'
 
 function App() {
   const [data, setData] = useState({})
-  const [location, setLocation] = useState('Chandigarh')
+  const [location, setLocation] = useState('New Delhi')
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
 
